2dba4209e7926fe6bed878f75427b52d2e1013c3
cjwalle97 <cjwalle97@gmail.com>
2017-02-02 16:23:53 -0600

    Update
    Update

--- Assets/Scripts/StateMachine/ChaseState.cs
+++ Assets/Scripts/StateMachine/ChaseState.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ChaseState : IEnemyState
 {
+    private readonly StatePatternEnemy enemy;
+    private float searchTimer;
+
+    public ChaseState(StatePatternEnemy statePatternEnemy)
+    {
+        enemy = statePatternEnemy;
+    }
     public void UpdateState()
     {
-
+        Look();
+        Chase();
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -20,11 +28,31 @@ public class ChaseState : IEnemyState
 
     public void ToAlertState()
     {
-
+        enemy.currentState = enemy.alertState;
     }
 
     public void ToChaseState()
     {
+        Debug.Log("Can't transition as the same State");
+    }
+
+    private void Look()
+    {
+        RaycastHit hit;
+        Vector3 enemyToTarget = ((enemy.chaseTarget.position + enemy.offset) - enemy.eyes.transform.position);
+        if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+        }
+        else
+        {
+            ToAlertState();
+        }
+    }
+    private void Chase()
+    {
+        enemy.meshRendererFlag.material.color = Color.red;
+        enemy.navMeshAgent.destination = enemy.chaseTarget.position;
 
     }
 }
