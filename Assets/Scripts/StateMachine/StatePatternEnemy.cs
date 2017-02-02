7d8137f7eed29fcd655df8eb599a95de997705b4
cjwalle97 <cjwalle97@gmail.com>
2017-02-01 18:01:33 -0600

    Update
    Update

---
+++ Assets/Scripts/StateMachine/StatePatternEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatePatternEnemy : MonoBehaviour
+{
+    public float searchingTurnSpeed = 120f;
+    public float searchingDuration = 4f;
+    public sightRange = 20f;
+    public Transform[] wayPoints;
+    public Transform eyes;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
